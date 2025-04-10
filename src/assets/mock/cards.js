debe96e2539b4d977ec27931fb69a29afc7dc258
Igor-Victor21 <igor.victorcontato@gmail.com>
2025-04-10 12:09:32 -0300

    Add files via upload

---
+++ src/assets/mock/cards.js
@@ -0,0 +1,18 @@
+import igor from '../images/img-igor.jpg'
+import java from '../images/java.png'
+import android from '../images/android.png'
+
+
+
+export const cards = [
+    {
+        text: "Igor Victor", img: igor, cardText: "Interessado em se tornar desenvolvedor back-end, iniciando com a linguagem principal Java, mas buscando abranger para outras linguagens de programação."
+    },
+    {
+        img: android, text: "Carreira" , cardText: "" 
+    },
+    {
+        text: "Linguagens", img: java
+    },
+    
+]
